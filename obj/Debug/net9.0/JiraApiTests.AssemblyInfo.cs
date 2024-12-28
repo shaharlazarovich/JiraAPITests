231861197dd943ae5702c6ddde78a98fc853c4da
unknown <shahar@ensuredr.com>
2024-12-28 07:40:57 +0200

    Commit after changing back to JiraAPITests

--- obj/Debug/net9.0/JiraApiTests.AssemblyInfo.cs
+++ obj/Debug/net9.0/JiraApiTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JiraApiTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+925346240e474e4244356da82640de1b47fd215b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+559d14508970156fcda7b23626ee08db843c2b1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("JiraApiTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JiraApiTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
